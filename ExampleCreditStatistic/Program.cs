2747568b48576ecb05b5eb41c7d79c5d7df9d082
dylan2013 <dylan.huang@ischool.com.tw>
2013-07-23 13:11:28 +0800

    學分統計表範例程式

---
+++ ExampleCreditStatistic/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using SmartSchool.Customization.PlugIn;
+
+namespace ExampleCreditStatistic
+{
+    public class Program
+    {
+        [MainMethod()]
+        static public void Main()
+        {
+            new CreditStatistic(); //學分統計表
+        }
+    }
+}
